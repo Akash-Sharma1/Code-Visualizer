61afb9ca5a494138479cd023e5b516154c9bf939
Akash Sharma <aakashsharmajc100@gmail.com>
2020-03-02 15:21:04 +0530

    Index page updated

--- codevisualizer/codevis/static/datastructures.js
+++ codevisualizer/codevis/static/datastructures.js
@@ -1,63 +1,66 @@
-function array(x,y,s,cid){
-    this.x=x;
-    this.y=y;
-    this.s=s;
-    this.cid=cid;
-    this.size=0;
-    this.add_block = function(){
-      this.size+=1;
-      var canvas = document.getElementById(this.cid);
-      if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        //ctx.fillStyle = 'rgb(204, 204, 204)';
-        ctx.fillRect(this.x, this.y, this.s, this.s);
-        this.x = this.x + this.s +25;
-      }
-    }
-    this.makearray = function(size){
-      for(i=0;i<size;i++){
-        this.add_block();
-      }
+function block(x,y,s,cid){
+  this.x=x;
+  this.y=y;
+  this.s=s;
+  this.cid=cid;
+  this.show = function(){
+    var canvas = document.getElementById(this.cid);
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      ctx.fillStyle = 'rgb(204, 204, 204)';
+      ctx.fillRect(this.x, this.y, this.s, this.s);
     }
   }
-  function arrow(x,y,leap,cid,len){
-    this.x=x;
-    this.y=y;
-    this.len=len;
-    this.leap=leap;
-    this.cid=cid;
-    this.add_arrow = function(){  
-      var canvas = document.getElementById(this.cid);
-      if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        console.log(this.x)
-        ctx.beginPath();
-        ctx.moveTo(this.x + this.len, this.y+5);
-        ctx.lineTo(this.x + this.len + 15, this.y);
-        ctx.lineTo(this.x + this.len, this.y-5);
-        ctx.fill();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x + this.len, this.y);
-        ctx.stroke();
-      }
-      this.x = this.x + this.len + 15 + this.leap;
+}
+function block_n_arrow(x,y,s,cid){
+  this.x=x;
+  this.y=y;
+  this.s=s;
+  this.cid=cid;
+  this.len=10
+  // arrow length+ tip = 10+15 =25
+  this.show = function(){
+    var canvas = document.getElementById(this.cid);
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      ctx.fillStyle = 'rgb(204, 204, 204)';
+      ctx.fillRect(this.x, this.y, this.s, this.s);
+      ctx.beginPath();
+      var mid = this.y+(this.s/2);
+      var blckend = this.x + this.s;
+      ctx.moveTo(blckend + this.len, mid+5);
+      ctx.lineTo(blckend + this.len + 15, mid);
+      ctx.lineTo(blckend + this.len, mid-5);
+      ctx.fill();
+      ctx.moveTo(blckend , mid);
+      ctx.lineTo(blckend + this.len, mid);
+      ctx.stroke();
     }
   }
-  function linkedlist(x,y,s,cid){
-    this.x=x;
-    this.y=y;
-    this.s=s;
-    this.cid=cid;
-    this.arrow = new arrow(this.x + this.s , this.y + this.s/2 , this.s , 'canvas',10);
-    this.add_block = function(){
-      var canvas = document.getElementById(this.cid);
-      if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        //ctx.fillStyle = 'rgb(204, 204, 204)';
-        ctx.fillRect(this.x, this.y, this.s, this.s);
-        this.arrow.add_arrow();
-        this.x = this.x + this.s + 25;
-      }
-    }
+}
+
+
+function clear_canvas(cid){
+  var c = document.getElementById(cid);
+  var ctx = c.getContext("2d");
+  ctx.clearRect(0, 0, c.height, c.width);
+}
+
+function array(n,j){
+  var arr = [];
+  for(i=0;i<n;i++){
+    newp = new block(i*52,52*j,50,'canvas');
+    //newp.show();
+    arr.push(newp);
   }
-  
+  return arr;
+}
+function linked_list(n,j){
+  var ll = [];
+  for(i=0;i<n;i++){
+    newp = new block_n_arrow(i*75,52*j,50,'canvas');
+    //newp.show();
+    ll.push(newp);
+  }
+  return ll;
+}
--- codevisualizer/codevis/views.py
+++ codevisualizer/codevis/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
+    if request.method=='POST':
+        code = request.POST['code']
+        num = int( request.POST['num'] ) # no of arrays to be tracked
+        
+        arrays = []# name of arrays to be tracked
+
+        for i in range(num):
+            arr = request.POST[str(i)]
+            arrays.append(arr) 
+        
+         
+        
+        final=""       
+        f = open(r"codevis/static/demofile.js", "w")
+        f.write(final)
+        f.close()
+            
+        return render(request,'codevis/show.html')
     return render(request, 'codevis/index.html',{})
