b8a14cc2f09744ef61acb5dee1efcec45354cb4e
tanmay2904 <tanmayagrawal613@gmail.com>
2020-03-04 03:25:32 +0530

    Added code for formatting input code

---
+++ codevisualizer/codevis/cppupd.py
@@ -0,0 +1,141 @@
+def comment_cout(code):
+    ln = code.splitlines(True)
+    final = ""
+    i = 0
+    while i < len(ln):
+        t = ln[i].find("cout")
+        if t != -1:
+            temp = ln[i][:t] + "//"
+            t2 = ln[i][t:].find(';')
+            if(t2 != -1):
+                temp += ln[i][t:t+t2+1]
+                ln.insert(i+1,ln[i][t+t2+1:])
+            ln[i] = temp
+            #i = i[:t] + "//" + i[t:]
+        final += ln[i]
+        #fo.write(ln[i])
+        i+=1
+    return final
+
+def sub_inarray(code,arrays,i,symbols):
+    final=""
+    if code[i] in symbols:
+        return "-1"
+    while(i < len(code)):
+        if alpha(code[i]):
+            final+=code[i]
+        else:
+            break
+        i+=1
+    if final in arrays:
+        return final
+    return "-1"
+
+
+def is_valid_variable(a,b,symbols):
+    if a in symbols and b in symbols:
+        return True
+    return False
+
+def alpha(i):
+    if (ord(i)>=97 and ord(i)<97+26) or (ord(i)>=65 and ord(i)<65+26) or (ord(i)>=48 and ord(i)<48+10):
+        return True
+    return False
+
+def checkarrays(code,arrays):
+    dic = ["0"]*len(code)
+    symbols = [' ', '\n', ';', '<', '>', ',', '.', '(' ,')', '{' ,'}', '[' , ']', '+', '*', '-', '/', '^', '=', '&', '%' ,'!', '|' ,]
+    for i in range(len(code)):
+        x = sub_inarray(code,arrays,i,symbols)
+        if x!="-1":
+            if is_valid_variable(code[i-1],code[i+len(x)],symbols):
+                dic[i]=x
+                #potential out of bound
+    return dic
+
+def checkupdates(code,dic):
+    for i in range(len(code)):
+        if dic[i] != "0":
+            flag="0"
+            j=i
+            while(j < len(code) and j!='\n' and j!=';'):
+                if code[j]=='=' or code[j]=='.':
+                    flag=dic[i]
+                j+=1
+                #potential == failure case
+            dic[i]=flag
+    return dic
+
+
+def add_freeopen_after_main(code):
+    for i in range(len(code)):
+        final="int main"
+        x=i
+        y=0
+        while(x<len(code) and y<len(final) and final[y]==code[x]):
+            x+=1
+            y+=1
+        if y==len(final):
+            #check if (){ is there
+            j=x
+            while(j<len(code) and code[j]==' '):
+                j+=1#extra space
+            if code[j]!='(':
+                continue
+            
+            j+=1
+            while j<len(code) and (alpha(code[j]) or code[j]==','):
+                j+=1#args
+            if code[j]!=')':
+                continue
+            j+=1
+            while j<len(code) and (code[j]==' ' or code[j]==';'):
+                j+=1#extra space
+            if(code[j]!='{'):
+                continue
+            j+=1
+            return code[0:j]+"\nfreopen(\"output.txt\",\"w\",stdout);\n"+code[j+1:len(code)]
+    return "-1"
+    #code doeas not contains a "int main ( ) {", hence error should be returned
+
+def gen_define():
+    # final=""
+    # final+="#define print_visuals(arr) int visuals_count=0;"
+    # final+="for(int visuals_element=0;visuals_element<10;visuals_element++)"
+    # final+="{visuals_count++;}cout<<\" \"<<visuals_count<<endl;"
+    # final+="for(int visuals_element=0;visuals_element<10;visuals_element++){"
+    # final+="cout<<arr[visuals_element]<<\" \";}cout<<endl;"
+    # final+="\n"     
+    final="#define print_visuals(arr)"
+    final+="int visuals_count=0;"
+    final+="for(auto visuals_element:arr){visuals_count++;}"
+    final+="cout<<\" \""+"<<visuals_count<<endl;"
+    final+="for(auto visuals_element:arr){cout<<visuals_element<<\" \";}cout<<endl;"
+    final+="\n"       
+    return final
+  
+def gen_update(arr):
+    return "cout<<\""+arr+"\";\nprint_visuals("+arr+");\n\n"
+  
+def insert_update_statements(code,dic):
+    final=""
+    i=0
+    while i < len(code):
+        if dic[i]!="0":
+            j=i
+            while j<len(code) and code[j]!=';' and code[j]!='\n':
+                final+=code[j]
+                j+=1
+            if j < len(code) and code[j]==';':
+                final+=code[j]
+                j+=1
+            if j<len(code) and code[j]=='\n':
+                final+=code[j]
+                j+=1
+            final+=gen_update(dic[i])
+            i=j
+        else:
+            final+=code[i]
+            i+=1
+    return final        
+
--- codevisualizer/codevis/views.py
+++ codevisualizer/codevis/views.py
@@ -1,42 +1,182 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+import os
+from . import cppupd
+
+def change_cpp(code,arrays):
+    code = code.replace('\r','')
+    code=cppupd.comment_cout(code)#code for commenting cout on code recieved
+    dic = cppupd.checkarrays(code,arrays)#valid index of arrays in code which are upadting will have 1 in the dic
+    dic = cppupd.checkupdates(code,dic)
+    code=cppupd.insert_update_statements(code,dic)#changes ordering of dic
+    code=cppupd.gen_define()+code
+    code=cppupd.add_freeopen_after_main(code)#changes ordering of dic
+    # if function returned "-1" then code doesn't contains a "int main(){"
+    return code
+
+def correct_formatting(code):
+    ln = code.splitlines(True)
+    finalCode = ""
+    i = 0
+    while i < len(ln):
+        brack = 0
+        temp = ln[i]
+        textPresent = False
+        altered = False
+        text = temp.strip()
+        if(len(text) == 0):
+            i+=1
+            continue
+        for j in range(len(temp)):
+            if temp[j].isalnum():
+                textPresent = True
+            if temp[j] == '(':
+                brack+=1
+            elif temp[j] == ')':
+                brack-=1
+            elif temp[j] == ';':
+                if brack != 0:
+                    continue
+                ln.insert(i+1,temp[j+1:])
+                altered = True
+                temp = temp[:j+1]
+                break
+            elif temp[j] == '/' and textPresent and j < len(temp)-1 and temp[j+1] == '/':
+                ln.insert(i+1,temp[j:])
+                altered = True
+                temp = temp[:j]
+                break
+            elif temp[j] == '/' and textPresent == False and j < len(temp)-1 and temp[j+1] == '/':
+                break
+        finalCode+=temp
+        if altered:
+            finalCode+= '\n'
+        i+=1
+    #fo = open("codevis/code intercepted/test.cpp",'w')
+    #fo.write(finalCode)
+    return finalCode
+
+def format_loops(code):
+    ln = code.splitlines(True)
+    finalCode = ""
+    i = 0
+    #print(len(ln))
+    cnt = 0
+    while i < len(ln):
+        temp = ln[i]
+        if "for" in temp:
+            ini = temp.find("for")
+            j = ini + 3
+            temp = temp[:j] + temp[j:].strip()
+            if temp[j] == '(':
+                while temp[j] != ')':
+                    j+=1
+                temp2 = temp[j+1:].strip()
+                if(len(temp2) == 0):
+                    bracePresent = True
+                    for p in range(i+1,len(ln)):
+                        if len(ln[p].strip()) != 0:
+                            if ln[p].strip().find('{') != 0:
+                                bracePresent = False
+                            break
+                    if bracePresent == False:
+                        temp = temp[:j+1] + "{\n"
+                        cnt+=1
+                else:
+                    if temp2[0] != '{':
+                        ln.insert(i+1,temp[j+1:])
+                        temp = temp[:j+1] + "{\n"
+                        cnt+=1
+
+        elif "while" in temp:
+            ini = temp.find("while")
+            j = ini + 5
+            temp = temp[:j] + temp[j:].strip()
+            if temp[j] == '(':
+                print("HI")
+                while temp[j] != ')':
+                    j+=1
+                temp2 = temp[j+1:].strip()
+                if(len(temp2) == 0):
+                    bracePresent = True
+                    for p in range(i+1,len(ln)):
+                        if len(ln[p].strip()) != 0:
+                            if ln[p].strip().find('{') != 0:
+                                bracePresent = False
+                            break
+                    if bracePresent == False:
+                        temp = temp[:j+1] + "{\n"
+                        cnt+=1
+                else:
+                    if temp2[0] != '{':
+                        ln.insert(i+1,temp[j+1:])
+                        temp = temp[:j+1] + "{\n"
+                        cnt+=1
+
+        elif cnt != 0:
+            j = temp.find(';')
+            if j != -1:
+                temp = temp[:j+1] + '}'*cnt +'\n'+ temp[j+1:]
+                cnt = 0
+        finalCode += temp
+        i+=1
+    #print(finalCode)
+
+    fo = open("codevis/code intercepted/test.cpp",'w')
+    fo.write(finalCode)
+    return finalCode
 
 def index(request):
     if request.method=='POST':
         code = request.POST['code']
-        num = int( request.POST['num'] ) # no of arrays to be tracked
+        num = int( request.POST['num'] ) #no of arrays to be tracked
         lang = request.POST['lang']
-        arrays = []# name of arrays to be tracked
-        
-        #code for commenting cout on code recieved
-        
-        fo = open("codevis/code intercepted/source.cpp","w")
-        ln = code.splitlines(True)
-        final = ""
-        i = 0
-        while i < len(ln):
-            t = ln[i].find("cout")
-            if t != -1:
-                temp = ln[i][:t] + "//"
-                t2 = ln[i][t:].find(';')
-                if(t2 != -1):
-                    temp += ln[i][t:t+t2+1]
-                    ln.insert(i+1,ln[i][t+t2+1:])
-                ln[i] = temp
-                #i = i[:t] + "//" + i[t:]
-            final += ln[i]
-            #fo.write(ln[i])
-            i+=1
-        fo.write(final)
-       #the code ends here
-        
+        arrays = [] #name of arrays to be tracked
         for i in range(num):
             arr = request.POST[str(i)]
+            if arr == "":
+                continue
             arrays.append(arr) 
 
-         
-        
-        #subprocess.call(["g++","codevis//code intercepted//source.cpp"])
-        #subprocess.call("codevis//code intercepted//a.exe")
+        if lang=="C++":
+            code = correct_formatting(code) #separate semicolons with new lines, puts comments in new line
+            code = format_loops(code) #add braces to loops
+            code = change_cpp(code,arrays)
+            if code=="-1":
+                return HttpResponse("Invalid code")
             
-        return render(request,'codevis/show.html')
+            fo = open("codevis\code_intercepted\source.cpp","w")
+            fo.write(code)
+            fo.close()
+            
+            os.system("g++ -o codevis\\code_intercepted\\a codevis\\code_intercepted\\source.cpp")
+            os.system("codevis\\code_intercepted\\a.exe")
+            
+        fo = open ("output.txt","r")
+        lines=fo.readlines()
+        
+        final=[]
+        for num in range(0,len(lines),2):
+            line1 = lines[num].split()
+            line2 = lines[num+1].split()
+            array_name = line1[0].strip()
+            array_size = line1[0].strip()
+            array_elem=[]
+            for i in line2:
+                array_elem.append(i.strip())
+            final.append({
+                'arr_name': array_name,
+                'arr_size': array_size,
+                'arr_elem': array_elem, 
+            })
+            
+        return render(request,'codevis/show.html',{'out':final})
     return render(request, 'codevis/index.html',{})
+
+# #include <bits/stdc++.h>
+# using namespace std;
+# int main(){
+#     int arr[10]={0};
+#     for(int i=0;i<10;i++){
+#         arr[i]=1;
+#     }
+# }
